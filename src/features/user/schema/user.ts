37ca50a95934203825b293bcfad2778bb6a8428d
Topher-gacad <cgacad@cornersteel.com>
2025-02-17 11:20:31 +0800

    user create route and form

--- src/features/user/schema/user.ts
+++ src/features/user/schema/user.ts
@@ -11,7 +11,7 @@ export const UserSchema = z.object({
     .string()
     .min(8, "Password must be at least 8 characters")
     .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-    .regex(/[a-Z]/, "Password must contain at least one lowercase letter")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
     .regex(/[0-9]/, "Password must contain at least one number letter")
     .regex(
       /[^A-Za-z0-9]/,
